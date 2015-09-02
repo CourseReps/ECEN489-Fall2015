278c35cb4343fa1604f4c25fc859986a4592f7f9
tbranyon <thomas.branyon@gmail.com>
2015-09-02 13:09:21 -0500

    Task 2 files

---
+++ Students/tbranyon/Task2/CMakeFiles/Hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello.dir/helloworld.c.o"
+  "Hello.pdb"
+  "Hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
