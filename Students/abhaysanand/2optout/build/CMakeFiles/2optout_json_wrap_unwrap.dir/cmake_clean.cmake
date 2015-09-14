03560b634616cb910117572f93c5cd987ca3c84b
Abhay Shankar Anand <abhaysanand@tamu.edu>
2015-09-14 06:05:54 -0500

    optout2

---
+++ Students/abhaysanand/2optout/build/CMakeFiles/2optout_json_wrap_unwrap.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2optout_json_wrap_unwrap.dir/json_curl_optout2.cpp.obj"
+  "CMakeFiles/2optout_json_wrap_unwrap.dir/jsoncpp.cpp.obj"
+  "CMakeFiles/2optout_json_wrap_unwrap.dir/stdafx.cpp.obj"
+  "CMakeFiles/2optout_json_wrap_unwrap.dir/HTTPDownloader.cpp.obj"
+  "2optout_json_wrap_unwrap.pdb"
+  "2optout_json_wrap_unwrap.exe"
+  "2optout_json_wrap_unwrap.exe.manifest"
+  "lib2optout_json_wrap_unwrap.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2optout_json_wrap_unwrap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
