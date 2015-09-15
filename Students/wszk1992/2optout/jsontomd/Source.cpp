7440d6249692d83379a99b12ad5695d51ff29506
Kan Zheng <wszk1992@gmail.com>
2015-09-14 19:50:38 -0500

    add optout2

---
+++ Students/wszk1992/2optout/jsontomd/Source.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ifstream jsonfile("rankingstennis.json");
+	stringstream input;
+	input << jsonfile.rdbuf();
+	string fileString = input.str();
+	cout << fileString << endl;
+	int pos = fileString.find("\n");
+	int pos1 = fileString.find("]");
+	string header = fileString.substr(1, pos - 4);
+	string sub = fileString.substr(pos, pos1 - pos);
+	vector<pair<string, string>> Pts;
+	for (int i = 1; i<7; i++)
+	{
+		int pos2 = sub.find("Name");
+		int pos3 = sub.find("Rankings");
+		string name = sub.substr(pos2 + 7, pos3 - pos2 - 10);
+		string points = sub.substr(pos3 + 9, 4);
+		pair<string, string>Points(name, points);
+		Pts.push_back(Points);
+		sub = sub.substr(pos3 + 1, pos1 - pos3);
+	}
+	ofstream fileout("markdown.MD");
+	fileout << "#" << header << endl << endl;
+	fileout << "* " << "Name" << ",   " << "Rankings" << endl;
+	cout << "#" << header << endl << endl;
+	cout << "* " << "Name" << ",   " << "Rankings" << endl;
+	for (int i = 0; i<Pts.size(); i++)
+	{
+		fileout << "* " << Pts[i].first << ",   " << Pts[i].second << endl;
+		cout << "* " << Pts[i].first << ",   " << Pts[i].second << endl;
+	}
+	return 0;
+}
