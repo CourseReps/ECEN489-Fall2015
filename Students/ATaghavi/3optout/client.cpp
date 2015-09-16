6169f9db831abb266eee1655afcde1cac71553df
Austin Taghavi <austintaghavi@gmail.com>
2015-09-16 14:58:53 -0500

    chaning port for server

--- Students/ATaghavi/3optout/client.cpp
+++ Students/ATaghavi/3optout/client.cpp
@@ -22,16 +22,16 @@ int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 2)
+    if (argc != 3)
     {
-      std::cerr << "Usage: client <host>" << std::endl;
+      std::cerr << "Usage: client port" << std::endl;
       return 1;
     }
 
     boost::asio::io_service io_service;
 
     tcp::resolver resolver(io_service);
-    tcp::resolver::query query(argv[1], "daytime");
+    tcp::resolver::query query(argv[1], argv[2]);
     tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
     tcp::resolver::iterator end;
 
