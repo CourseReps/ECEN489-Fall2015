2d15850572e982f6b345ad1b0e919e829df44be3
Austin Taghavi <austintaghavi@gmail.com>
2015-09-30 11:47:55 -0500

    6optout

--- Students/ATaghavi/6optout/server.cpp
+++ Students/ATaghavi/6optout/server.cpp
@@ -19,22 +19,10 @@
 #include <stdlib.h>
 #include <sqlite3.h> 
 
-using boost::asio::ip::tcp;
+using boost::asio::ip::udp;
 using namespace std;
 
 sqlite3 *db;
-
-void take_screenshot(string photo_name)
-{
-  cout<<"taking photo...\n";
-  photo_name = "screencapture " + photo_name;
-  system(photo_name.c_str());
-}
-
-void take_webcam_shot()
-{
-  system("imagesnap -w 1.0");
-}
 
 string get_val_from_json(string val,string data)
 {
@@ -49,9 +37,11 @@ string get_val_from_json(string val,string data)
 vector< pair<string, string> > parse_json(string data)
 {
   vector< pair<string, string> > vals;
-  vals.push_back( pair<string,string>("photoVal", get_val_from_json("photoVal", data)) );
-  vals.push_back( pair<string,string>("ip", get_val_from_json("ip", data)) );
-  vals.push_back( pair<string,string>("timestamp", get_val_from_json("timestamp", data)) );
+  vals.push_back( pair<string,string>("DeviceID", get_val_from_json("DeviceID", data)) );
+  vals.push_back( pair<string,string>("DeviceType", get_val_from_json("DeviceType", data)) );
+  vals.push_back( pair<string,string>("Data", get_val_from_json("Data", data)) );
+  vals.push_back( pair<string,string>("Timestamp", get_val_from_json("Timestamp", data)) );
+  vals.push_back( pair<string,string>("currentIP", get_val_from_json("currentIP", data)) );
   return vals;
 }
 
@@ -61,9 +51,10 @@ void set_up_db()
   char *zErrMsg = 0;
   string sql;
   sql = "CREATE TABLE IF NOT EXISTS IP_DATA("  \
-         "firstName           TEXT    NOT NULL," \
-         "lastName            TEXT     NOT NULL," \
-         "gitUserName           TEXT    NOT NULL," \
+         "DeviceID           TEXT    NOT NULL," \
+         "DeviceType            TEXT     NOT NULL," \
+         "Data           TEXT    NOT NULL," \
+         "Timestamp           TEXT    NOT NULL," \
          "currentIP            TEXT     NOT NULL);";
 
    /* Execute SQL statement */
@@ -81,9 +72,9 @@ void add_to_database(vector<pair<string,string> > vec)
   int rc;
   char *zErrMsg = 0;
   string sql;
-  sql = "INSERT INTO IP_DATA (firstName,lastName,gitUserName,currentIP) "  \
-        "SELECT '" + vec[0].second + "' , '"+ vec[1].second + "' , '"+ vec[2].second + "' , '"+ vec[3].second + "' " \
-        "WHERE NOT EXISTS (SELECT firstName FROM IP_DATA WHERE firstName='"+vec[0].second+"' AND lastName='" + vec[1].second + "' AND currentIP='" + vec[3].second + "');";
+  sql = "INSERT INTO IP_DATA (DeviceID,DeviceType,Data,Timestamp,currentIP) "  \
+        "SELECT '" + vec[0].second + "' , '"+ vec[1].second + "' , '"+ vec[2].second + "' , '"+ vec[3].second + "' , '"+ vec[4].second + "' " \
+        "WHERE NOT EXISTS (SELECT DeviceID FROM IP_DATA WHERE DeviceID='"+vec[0].second+"' AND Timestamp='" + vec[3].second + "' AND currentIP='" + vec[4].second + "');";
 
   rc = sqlite3_exec(db, sql.c_str(), NULL, 0, &zErrMsg);
      if( rc != SQLITE_OK ){
@@ -94,27 +85,46 @@ void add_to_database(vector<pair<string,string> > vec)
      }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-  
   try
   {
+    int rc;
+    rc = sqlite3_open("6optout.db", &db);
+    set_up_db();
+
+    if (argc != 2)
+    {
+        std::cerr << "Usage: client port" << std::endl;
+        return 1;
+    }
+    unsigned short port = (unsigned short) strtoul(argv[1], NULL, 0);
     boost::asio::io_service io_service;
+    udp::socket sock(io_service, udp::endpoint(udp::v4(), port));
 
-    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 14));
-
+    if( rc ){
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      exit(0);
+    }else{
+      fprintf(stderr, "Opened database successfully\n");
+    }
+    int max_length = 128;
     for (;;)
     {
-      tcp::socket socket(io_service);
-      acceptor.accept(socket);
+      char data[max_length];
+        udp::endpoint sender_endpoint;
+        size_t length = sock.receive_from(
+            boost::asio::buffer(data, max_length), sender_endpoint);
+        
+        string json(data);
 
-      boost::array<char, 128> buf;
-      boost::system::error_code error;
+        std::string::iterator end_pos = std::remove(json.begin(), json.end(), ' ');
+        json.erase(end_pos, json.end());
 
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
-
-      string command(buf.data(), len);
-
+        vector< pair< string, string> > values = parse_json(data);
+        
+        add_to_database(values);
+      /*
       cout<<"Message: "<<command<<"\n";
       boost::system::error_code ignored_error;
 
@@ -127,8 +137,7 @@ int main()
       {
         boost::asio::write(socket, boost::asio::buffer("NOT"), boost::asio::transfer_all(), ignored_error);
       }
-
-      
+      */ 
     }
   }
   catch (std::exception& e)
