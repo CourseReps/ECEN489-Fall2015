0a509c6465ffcfb186565c5f06f2ceae7ed49e6b
travistag <travistag@gmail.com>
2015-10-12 13:55:24 -0500

    Updating client

--- Students/travistag/Project1/src/client.cpp
+++ Students/travistag/Project1/src/client.cpp
@@ -16,6 +16,7 @@
 #include <fstream>
 #include <stdlib.h>
 #include <ctime>
+#include <sqlite3.h>
 
 
 using boost::asio::ip::udp;
@@ -33,14 +34,6 @@ string getValue(string devi){
     return ret;
   }
   return ret;
-}
-
-void writeValue(int on){
-  if(on==1){
-    system("echo \"b\" > /dev/cu.usbmodem1062571");
-  }
-  else
-    system("echo \"a\" > /dev/cu.usbmodem1062571");
 }
 
 std::string getCurrentIP(){
@@ -67,9 +60,9 @@ std::vector<double> getDataFromString(std::string s){
     found=s.find(",");
     if(found!=std::string::npos){
       string temp = s.substr(0, found);
-      cout<<"temp: "<<temp<<"\n";
+//      cout<<"temp: "<<temp<<"\n";
       s=s.substr(found+1);
-      cout<<"new s: "<<s<<"\n";
+//      cout<<"new s: "<<s<<"\n";
       ret.push_back(stod(temp));
     }
     else
@@ -94,6 +87,30 @@ std::string getJSON(std::vector<std::string> stuff){
   return ret;
 }
 
+
+void addDeviceInfo(std::vector<std::string> inf, sqlite3 *db){
+  std::string comm = "INSERT INTO SensorData (Flowrate, Pumprate, IRsensor, Solenoid, Time)";
+  comm+=" SELECT "+inf[0]+", '"+inf[1]+"', "+inf[2]+", '"+inf[3]+"', '"+inf[4]+"'";
+  comm+=" WHERE NOT EXISTS (SELECT Pumprate FROM SensorData WHERE Flowrate='"+inf[0]+"' AND Pumprate='"+inf[1]+"' AND IRSensor='"+inf[2]+"' AND Solenoid='"+inf[3]+"' AND Time='"+inf[4]+"')";
+  char *err_msg = 0;
+
+  int rc = sqlite3_open("sensordata.db", &db);
+
+  if (rc != SQLITE_OK) {
+    std::cout<<"Cannot open database: "<<sqlite3_errmsg(db);
+    sqlite3_close(db);
+  }
+
+  rc = sqlite3_exec(db, comm.c_str(), 0, 0, &err_msg);
+  if (rc != SQLITE_OK) {
+
+    fprintf(stderr, "SQL error: %s\n", err_msg);
+
+    sqlite3_free(err_msg);
+  }
+  sqlite3_close(db);
+}
+
 int main(int argc, char* argv[])
 {
 
@@ -101,14 +118,42 @@ int main(int argc, char* argv[])
     std::cout<<"\nPlease enter a port # to broadcast on, adn device to read!\n\n";
     return 1;
   }
+  sqlite3 *db;
+  char *err_msg = 0;
+  sqlite3_stmt *stmt;
+  int rc = sqlite3_open("sensordata.db", &db);
+
+  if (rc != SQLITE_OK) {
+    std::cout<<"Cannot open database: "<<sqlite3_errmsg(db);
+    sqlite3_close(db);
+    return 1;
+  }
+
+  std::string com = "CREATE TABLE IF NOT EXISTS SensorData(Flowrate TEXT, Pumprate TEXT, IRSensor TEXT, Solenoid TEXT, Time TEXT)";
+  rc = sqlite3_exec(db, com.c_str(), 0, 0, &err_msg);
+  if (rc != SQLITE_OK) {
+
+    fprintf(stderr, "SQL error: %s\n", err_msg);
+
+    sqlite3_free(err_msg);
+  }
+  sqlite3_close(db);
+
+  while(true){
 
   string rawdat = getValue(argv[1]);
 
   vector<double> currentData = getDataFromString(rawdat);
+  std::vector<string> v;
   for(int i=0; i<currentData.size(); i++){
     cout<<currentData[i]<<"\n";
+    v.push_back(to_string(currentData[i]));
   }
+  v.push_back(to_string(time(0)));
+  addDeviceInfo(v, db);
   cout<<"\n\n";
+  sleep(3);
+}
   /*string myIP=getCurrentIP();
 
   try
