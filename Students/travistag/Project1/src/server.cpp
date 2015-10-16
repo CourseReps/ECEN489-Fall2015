4a21a4df36d787600bfd8be5f23fff159d0229f0
travistag <travistag@gmail.com>
2015-10-16 11:24:38 -0500

    updating server

--- Students/travistag/Project1/src/server.cpp
+++ Students/travistag/Project1/src/server.cpp
@@ -8,10 +8,81 @@
 using boost::asio::ip::tcp;
 using namespace std;
 
+string getValue(string devi){
+  string ret="NULL";
+  string comm = "sudo head -1 /dev/rfcomm0 > read.txt";
+  system(comm.c_str());
+  ifstream myinput("read.txt");
+  if(myinput.is_open()){
+    string line;
+    if (getline(myinput, line))
+      ret = line;
+    return ret;
+  }
+  return ret;
+}
+
+void writeValue(int x){
+  system("echo "+x+" | sudo tee /dev/rfcomm0");
+}
+
+std::string getCurrentIP(){
+  system("ifconfig en0 | grep \"inet \" > currentip.txt");
+  ifstream myinput("currentip.txt");
+  std::string line;
+  if(myinput.is_open()){
+      getline(myinput, line);
+      line = line.substr(line.find_first_of("inet")+5);
+      line = line.substr(0, line.find_first_of(" "));
+      myinput.close();
+      if(line.length()<8)
+        return "0.0.0.0";
+      return line;
+  }
+  else
+    return "0.0.0.0";
+}
+
+std::vector<double> getDataFromString(std::string s){
+  int found=0;
+  std::vector<double> ret;
+  do{
+    found=s.find(",");
+    if(found!=std::string::npos){
+      string temp = s.substr(0, found);
+//      cout<<"temp: "<<temp<<"\n";
+      s=s.substr(found+1);
+//      cout<<"new s: "<<s<<"\n";
+      ret.push_back(stod(temp));
+    }
+    else
+      ret.push_back(stod(s));
+  }
+  while(found!=std::string::npos);
+
+  return ret;
+}
+
+std::string getJSON(std::vector<std::string> stuff){
+  std::string ret;
+
+  ret+="{ ";
+  ret+="\"ID\": \""+stuff[0]+"\", ";
+  ret+="\"IRRange\": \""+stuff[1]+"\", ";
+  ret+="\"PumpRate\": \""+stuff[2]+"\", ";
+  ret+="\"FlowRate\": \""+stuff[3]+"\", ";
+  ret+="\"SolenoidState\": \""+stuff[3]+"\", ";
+  ret+="\"Timestamp\": \""+stuff[3]+"\", ";
+  ret+="\"CurrentIP\": \""+stuff[4]+"\" ";
+  ret+="}";
+
+  return ret;
+}
+
 
 int main(){
   boost::asio::io_service io_service;
-  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 2222));
   while(true)
     {
       tcp::socket socket(io_service);
@@ -25,17 +96,28 @@ int main(){
       std::string comm(buf.begin(), buf.end());
       string message;
       std::cout<<"Received command: "<<comm<<"\n";
-      if(comm.compare("get_data")){
+      if(comm.substr(0,8).compare("get_data")==0){
         cout<<"Reading from head, packing into JSON and sending!\n\n";
-        message = "THE JSON OBJECT";
+        getValue("rfcomm0");
+        string vals = getValue("rfcomm0");
+        vector<double> currentData = getDataFromString(vals);
+        std::vector<string> v;
+        v.push_back("Team4");
+        v.push_back(to_string(currentData[2]));
+        v.push_back(to_string(currentData[0]));
+        v.push_back(to_string(currentData[1]));
+        v.push_back(to_string(currentData[3]));
+        v.push_back(to_string(time(0)));
+        v.push_back(getCurrentIP());
+        message = getJSON(v);
       }
       else{
         try{
-        int x = atoi(comm);
+        int x = atoi(comm.c_str());
         cout<<"Setting pump to be "<<x<<"\n\n";
         message = "OK";
       }
-      catch(Exception e){
+      catch(exception e){
         cout<<"Voltage value must be an int!\n\n";
         message = "FAIL";
       }
