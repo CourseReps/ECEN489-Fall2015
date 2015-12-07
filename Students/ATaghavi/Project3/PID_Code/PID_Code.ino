259dce0cf8fd7e8a89ed8ba30a4bd64f7bfe62ea
Austin Taghavi <austintaghavi@gmail.com>
2015-12-07 14:10:57 -0600

    Adding PId code

--- Students/ATaghavi/Project3/PID_Code/PID_Code.ino
+++ Students/ATaghavi/Project3/PID_Code/PID_Code.ino
@@ -7,7 +7,7 @@ double Setpoint, Input, Output;
 //Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint,2.3,5,0, DIRECT);
 
-int MotorPin = 6;
+int MotorPin = 3;
 int IRPin = A0;
 unsigned long start_t = 0;
 unsigned long end_t = 0;
@@ -15,12 +15,16 @@ SoftwareSerial BTSerial(9,10);
 int solenoidState = 1;
 volatile int NbTopsFan;
 int Calc; 
-int hallsensor = 4;
+int hallsensor = 5;
 int steadyCount = 0;
 float lastVal = 0.0;
 float threshold = 1.0;
-int steadyThreshold = 10;
+float currentVolume = 0.0;
+int steadyThreshold = 5;
 bool isSteady = false;
+bool measureVolume = false;
+float volume = 0.0;
+float steadyFlowRate = 35.0;
 void rpm ()     //This is the function that the interupt calls 
 { 
  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
@@ -33,10 +37,14 @@ void setup()
   Input = analogRead(IRPin);
   Setpoint = getAnalogValue(10);
   BTSerial.begin(9600);
-  
+  Serial.begin(9600);
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
   attachInterrupt(hallsensor, rpm, RISING); //and the interrupt is attached
+
+  //turn on LED
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
 }
 
 int getHeight()
@@ -64,18 +72,20 @@ void loop()
  {
  // end_t = millis();
   cli();
-  Calc = (NbTopsFan * 60 / 7.5)/(1);
+  Calc = (NbTopsFan * 60 / 7.5)/(0.5);
   sei();
   //start_t = millis();
   numLoops = 0;
   NbTopsFan = 0; 
-  Serial.println(Calc, DEC);
-  if(Calc - lastVal) > threshold) {
+  //Serial.println(Calc, DEC);
+  if((Calc - lastVal) > threshold) {
     steadyCount = 0;
     
     if(isSteady)
     {
-      //startCounting
+      volume = 0.0;
+      currentVolume = 0.0;
+      measureVolume = true;
     }
     
     isSteady = false;
@@ -85,11 +95,17 @@ void loop()
      if(steadyCount > steadyThreshold){
        if(!isSteady) {
          //stop counting, send estimated volume
+         measureVolume = false;
+         currentVolume = volume;
        }
        isSteady = true;
      }
        
    }
+   if(measureVolume) {
+      volume += (Calc-steadyFlowRate);
+   }
+    
  }
   //If serial is available, get input and convert to actual value to set
   if(Serial.available())
@@ -100,8 +116,13 @@ void loop()
   }
   
   analogWrite(MotorPin,map(Output,0,255,0,120));
+  Serial.print(volume);
+  Serial.print(" , ");
+  Serial.print(Calc,DEC);
+  Serial.print(" , ");
+  Serial.println( steadyCount);
   //analogWrite(MotorPin,0);
   //Serial.println(map(Output,0,255,0,60));
   numLoops ++;
-  delay(100);
+  delay(50);
 }
