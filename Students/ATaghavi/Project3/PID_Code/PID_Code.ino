b326c29f247cc8126f3e8a49035ca8b63c806ca6
Austin Taghavi <austintaghavi@gmail.com>
2015-12-07 14:23:52 -0600

    Adding PId code

--- Students/ATaghavi/Project3/PID_Code/PID_Code.ino
+++ Students/ATaghavi/Project3/PID_Code/PID_Code.ino
@@ -5,10 +5,10 @@
 double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint,2.3,5,0, DIRECT);
+PID myPID(&Input, &Output, &Setpoint,80,3,2, DIRECT);
 
 int MotorPin = 3;
-int IRPin = A0;
+int IRPin = 15;
 unsigned long start_t = 0;
 unsigned long end_t = 0;
 SoftwareSerial BTSerial(9,10);
@@ -17,16 +17,22 @@ volatile int NbTopsFan;
 int Calc; 
 int hallsensor = 5;
 int steadyCount = 0;
-float lastVal = 0.0;
-float threshold = 1.0;
+float lastVal = 0;
+float threshold = 0.5;
 float currentVolume = 0.0;
-int steadyThreshold = 5;
+float steadyAvg = 0.0;
+int steadyThreshold = 10;
 bool isSteady = false;
 bool measureVolume = false;
 float volume = 0.0;
-float steadyFlowRate = 35.0;
+float steadyFlowRate = 850.0;
+float firstSteadyVal = 0.0;
+
+float heights[5];
+
 void rpm ()     //This is the function that the interupt calls 
 { 
+ //Serial.println("*");
  NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
 } 
 
@@ -34,8 +40,9 @@ void setup()
 {
   //initialize the variables we're linked to
   pinMode(hallsensor, INPUT);
-  Input = analogRead(IRPin);
-  Setpoint = getAnalogValue(10);
+  
+  Input = getHeightAvg();
+  Setpoint = 10;
   BTSerial.begin(9600);
   Serial.begin(9600);
   //turn the PID on
@@ -47,38 +54,93 @@ void setup()
   digitalWrite(6, HIGH);
 }
 
+double getHeight2(){
+  int rea = getHeight();
+  //Serial.println(rea);
+  if(rea>0){
+    double re = rea/1.0;
+    double hei = 8283.4*pow(re, -1.334);
+    return 17-hei;
+  }
+  else
+    return 0;
+}
+
+float getHeightAvg() {
+  
+  float sum = 0.0;
+  int high = 0;
+  int low = 10000;
+  for(int i=0; i<5; i++)
+  {
+    if(heights[i] > high)
+      high = heights[i];
+    if(heights[i] < low)
+      low = heights[i];
+    sum+=heights[i];
+  }
+  
+  //sum -= high+low;
+  return (float)sum/5.0;
+}
+
 int getHeight()
 {
   return analogRead(IRPin);
 }
 
-float getAnalogValue(int val)
+float getAnalogValue(float val)
 {
   return (91.16084 + 40.91384*val -7.943432*val*val + 0.6022727*val*val*val);
 }
 
 int numLoops = 10;
 
+void updateHeights()
+{
+  /*
+  heights[0] = heights[1];
+  heights[1] = heights[2];
+  heights[2] = heights[3];
+  heights[3] = heights[4];
+  */
+  
+  for(int i=0; i<4; i++) {
+    heights[i] = heights[i+1];
+  }
+  
+  heights[4] = getHeight2();
+}
+
 void loop()
 {
-  Input = analogRead(IRPin);
+  updateHeights();
+  
+  Input = getHeightAvg();
   //Serial.println(Input);
   myPID.Compute();
-  /*
-  sei();            //Enables interrupts
- cli();
- */
- if(numLoops == 10)
+  
+  
+  
+ if(numLoops >= 500 )
  {
  // end_t = millis();
   cli();
-  Calc = (NbTopsFan * 60 / 7.5)/(0.5);
+  //delay(1000);
+  Calc = (NbTopsFan * 60 / 7.5)*2;
   sei();
+  Calc = (NbTopsFan * 60 / 7.5)*2;
+  //Serial.println(Calc,DEC);
   //start_t = millis();
   numLoops = 0;
   NbTopsFan = 0; 
   //Serial.println(Calc, DEC);
-  if((Calc - lastVal) > threshold) {
+  float heightAvg = getHeightAvg();
+  /*
+  if( (!isSteady &&(steadyCount == 0 && abs( Input - lastVal) > threshold ) || (steadyCount > 0 && abs(Input - firstSteadyVal) > threshold)  ) || 
+      (isSteady && abs(Input - steadyAvg) > threshold) ) {
+        */
+  if(abs(heightAvg - Setpoint) > threshold) {
     steadyCount = 0;
     
     if(isSteady)
@@ -91,38 +153,47 @@ void loop()
     isSteady = false;
    }
    else {
+     if(steadyCount == 0)
+     {
+       firstSteadyVal = heightAvg;
+     }
      steadyCount ++;
+     measureVolume = false;
+     currentVolume = volume/24.5;
      if(steadyCount > steadyThreshold){
-       if(!isSteady) {
-         //stop counting, send estimated volume
-         measureVolume = false;
-         currentVolume = volume;
-       }
        isSteady = true;
      }
-       
+      
    }
    if(measureVolume) {
-      volume += (Calc-steadyFlowRate);
+      volume -= (Calc-steadyFlowRate)/2;
    }
-    
+   lastVal = heightAvg;
  }
   //If serial is available, get input and convert to actual value to set
   if(Serial.available())
   {
     int t = Serial.parseInt();
     if(t >=5 && t<=15)
-      Setpoint = getAnalogValue(t);
+      Setpoint = t;
   }
   
-  analogWrite(MotorPin,map(Output,0,255,0,120));
-  Serial.print(volume);
-  Serial.print(" , ");
-  Serial.print(Calc,DEC);
-  Serial.print(" , ");
-  Serial.println( steadyCount);
+  analogWrite(MotorPin,map(Output,0,255,0,150));
+  
+  Serial.print("Team2");
+  Serial.print(",");
+  Serial.print(Input);
+  Serial.print(",");
+  Serial.print(Output);
+  Serial.print(",");
+  Serial.print( Calc/24.5, DEC );
+  Serial.print(",");
+  Serial.print( currentVolume);
+  Serial.print(",");
+  Serial.println(isSteady);
+  
   //analogWrite(MotorPin,0);
   //Serial.println(map(Output,0,255,0,60));
   numLoops ++;
-  delay(50);
+  delay(1);
 }
